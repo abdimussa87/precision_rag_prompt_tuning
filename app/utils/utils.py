e00ee06a84bdf62df371f316bc3f3ed837578cc8
Abdi Mussa <abdimussa87@gmail.com>
2024-01-20 22:25:10 +0300

    feat: utilized pypdf2 to read the pdf's sent as a context

---
+++ app/utils/utils.py
@@ -0,0 +1,33 @@
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = Chroma.from_documents(docs, embeddings)
+
+    return vectorstore
+
+
+def get_retriever(vectorstore):
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
+    return retriever
